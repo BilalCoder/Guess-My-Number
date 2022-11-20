35e5edffac2d09e78bcfec75ce66cb8fc40c55a3
Bilal Khan <bilal.khan@publicissapient.com>
2022-11-21 00:04:14 +0530

    logic added for win and lose for guess my number game

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 6) + 1;
+let secretNumber = Math.trunc(Math.random() * 10) + 1;
 let highScore = 0;
+let score = 6;
 
 document.querySelector('.left .check').addEventListener('click', function () {
-    let enteredNumber = Number(document.querySelector('guess').value);
+    let enteredValue = document.querySelector('.guess').value;
+    let enteredNumber = Number(enteredValue);
     let message = document.querySelector('.right .message');
-    if (enteredNumber == secretNumber) {
-        message.textContent = 'You Won ';
+    if (!enteredValue) {
+        message.textContent = 'Please enter a valid number';
+    }
+    else if (enteredNumber == secretNumber) {
+        message.textContent = 'You Won';
         document.querySelector('body').style.backgroundColor = 'green';
+        document.querySelector('.guess').disabled = true;
+        if(score > highScore) {
+            highScore = score;
+            document.querySelector('.right .highscore').textContent = highScore;
+        }
     } else if (enteredNumber != secretNumber) {
-        if(enteredNumber > secretNumber)
-            message.textContent = 'Number too high';
-        else 
-            message.textContent = 'Number too low';
-        
+        if (score > 1) {
+            if (enteredNumber > secretNumber) message.textContent = 'Number too high';
+            else message.textContent = 'Number too low';
+            score--;
+            document.querySelector('.right .score').textContent = score;
+        } else {
+            message.textContent = 'You lost the game';
+            document.querySelector('.right .score').textContent = 0;
+        }
     }
 });
